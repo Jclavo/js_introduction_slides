ed2b80ae01ef8450a92253a4d01953f12ca80ac8
jose.tafur1 <jose.tafur@mds.gov.br>
2026-09-21 11:22:08 -0300

    [feat]: example of a registration site (html + css + js) - Add max to textarea

--- snippet/day1/html_css_javascript/script.js
+++ snippet/day1/html_css_javascript/script.js
@@ -4,10 +4,17 @@ document.getElementById('registrationForm').addEventListener('input', function()
     const email = document.getElementById('email').value;
     const career = document.getElementById('career').value;
     const comment = document.getElementById('comment').value;
+    const charCountSpan = document.getElementById('charCount');
     
-    // Enable the button only if all required fields are filled and meet length requirements
+    // Update character count
+    charCountSpan.innerText = comment.length + '/200';
+    
+    // Change textarea color if over 200 characters
+    comment.length > 200 ? document.getElementById('comment').style.borderColor = 'red' : document.getElementById('comment').style.borderColor = '';
+    
+    // Enable the button only if all required fields are filled
     const registerButton = document.getElementById('registerButton');
-    registerButton.disabled = !name || !cpf || !email || (career.length > 50) || (comment.length > 200);
+    registerButton.disabled = !name || !cpf || !email || (career.length > 50);
 });
 
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
