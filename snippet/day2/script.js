e41ce5da3e9be625b6dcf93ce5d6defd9db7de3c
jose.tafur1 <jose.tafur@mds.gov.br>
2026-09-22 16:03:39 -0300

    [wip]

--- snippet/day2/script.js
+++ snippet/day2/script.js
@@ -1,1 +1,22 @@
-alert("Day2")
+// const myname = prompt("Ingresse seu nome");
+
+// alert("Benvindo " + myname)
+
+// let temperatura = 40;
+const temperatura = prompt("Ingresse a temperatura");
+
+if (temperatura < 15) {
+    alert("Está fazendo frio");
+}
+
+if(temperatura > 15 && temperatura < 30) {
+    alert("Está tudo bom");
+}
+
+if (temperatura > 30) {
+  alert("Está calor hoje");
+}
+
+if (temperatura >= 40) {
+  alert("Isso não deveria acontecer");
+}
