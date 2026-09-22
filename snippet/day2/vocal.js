b3ab0fe01d7c8d587494fac3735874ff3d75ca03
jose.tafur1 <jose.tafur@mds.gov.br>
2026-09-22 17:00:24 -0300

    [wip]

---
+++ snippet/day2/vocal.js
@@ -0,0 +1,25 @@
+/**
+ * Computar se a letra ingressada é uma vocal: 
+ * ENTRADA: Letra
+ * LOGICA: 
+ *  * LOGICA: 
+ *    - ???
+ * SAIDA: memsagem
+ */
+
+let letra = prompt("letra");
+
+if (letra == "a" || letra == "e" 
+    || letra == "i" || letra == "o" 
+    || letra == "u") 
+{
+    alert("é vogal");
+}
+
+if (letra != "a" && letra != "e" 
+    && letra != "i" && letra != "o" 
+    && letra != "u") 
+{
+    alert("não é uma vogal");
+}
+
