0b77e3181f682d165e5cd5337d4e4dced3173b48
jose.tafur1 <jose.tafur@mds.gov.br>
2026-09-23 17:56:24 -0300

    [fix]: remove blank lines

--- snippet/day2/exercicio_mencao_unb/if_else_if/mencao_unb.js
+++ snippet/day2/exercicio_mencao_unb/if_else_if/mencao_unb.js
@@ -4,20 +4,15 @@ const nota = prompt("Ingresse sua nota");
 if (nota < 0 || nota > 10) {
     alert("Valor inválido");
 }
-
-// Processar valores corretos
 else if (nota < 5) {
     alert("Sua menção é: reprovado");
 }
-
 else if (nota < 7) {
     alert("Sua menção é: MM");
 }
-
 else if (nota < 9) {
     alert("Sua menção é: MS");
 }
-
 else {
     alert("Sua menção é: SS");
 }
