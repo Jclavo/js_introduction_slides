b152d4c54488f2ce41af16afcc6efe0a7644dad7
jose.tafur1 <jose.tafur@mds.gov.br>
2026-09-24 11:24:51 -0300

    [feat]: day4 - task - logjn

---
+++ snippet/day4/login/script.js
@@ -0,0 +1,8 @@
+const EMAIL_CERTO = "pepe@unb.br";
+const SENHA_CERTO = "wannacry";
+
+let entrar = document.getElementById("botao-entrar");
+
+entrar.addEventListener("click", function () {
+  alert("Implementa alguma coisa aqui !!!!");
+});
